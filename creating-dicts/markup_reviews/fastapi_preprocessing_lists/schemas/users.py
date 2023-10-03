5263e8c808e946b85a9f009ea225db247943119e
artem12345-png <78581804+artem12345-png@users.noreply.github.com>
2023-10-03 18:40:08 +0700

    Delete creating-dicts directory

--- creating-dicts/markup_reviews/fastapi_preprocessing_lists/schemas/users.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel, SecretStr
-
-
-class User(BaseModel):
-    username: str
-    password_hash: SecretStr
--- creating-dicts/markup_reviews/fastapi_preprocessing_lists/services/creditionals.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from dotenv import load_dotenv
-
-load_dotenv()
-
-DB = {os.getenv("USERNAME"): {"password": os.getenv("PASSWORD")}}
-print(DB)
